d726b8deae9b221425359b526cdcd958eb6517a2
Tom Van Herreweghe <tom@theanalogguy.be>
2016-10-12 12:26:35 +0200

    Unit tests for all methods in FieldMapperTrait
    Signed-off-by: Tom Van Herreweghe <tom@theanalogguy.be>

--- tests/Mapper/FieldMapperTraitTest.php
+++ tests/Mapper/FieldMapperTraitTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\PHPExif\Common\Mapper;
 
-use BetterReflection\Reflection\ReflectionProperty;
 use Mockery as m;
+use PHPExif\Common\Exception\Mapper\MapperNotRegisteredException;
 use PHPExif\Common\Mapper\FieldMapper;
 use PHPExif\Common\Mapper\FieldMapperTrait;
 
@@ -70,4 +70,95 @@ class FieldMapperTraitTest extends \PHPUnit_Framework_TestCase
             $mock->mapperRegisteredForField('foo')
         );
     }
+
+    /**
+     * @covers ::mapperRegisteredForField
+     * @group mapper
+     *
+     * @return void
+     */
+	public function testMapperRegisteredForFieldReturnsFalseWhenMapperNotAdded()
+	{
+        $mock = $this->getMockBuilder(FieldMapperTrait::class)
+            ->getMockForTrait();
+
+		$this->assertFalse($mock->mapperRegisteredForField('foo'));
+	}
+
+    /**
+     * @covers ::mapperRegisteredForField
+     * @group mapper
+     *
+     * @return void
+     */
+	public function testMapperRegisteredForFieldReturnsTrueWhenMapperIsAdded()
+	{
+        $mock = $this->getMockBuilder(FieldMapperTrait::class)
+            ->getMockForTrait();
+
+
+        $mapper = $this->getMockBuilder(FieldMapper::class)
+            ->setMethods(['getSupportedFields', 'mapField'])
+            ->getMock();
+
+        $mapper->expects($this->once())
+            ->method('getSupportedFields')
+            ->will(
+                $this->returnValue(array('foo'))
+            );
+
+        $mock->registerFieldMapper($mapper);
+
+        $this->assertTrue(
+            $mock->mapperRegisteredForField('foo')
+        );
+	}
+
+    /**
+     * @covers ::getFieldMapper
+     * @group mapper
+     *
+     * @return void
+     */
+	public function testGetFieldMapperThrowsExceptionForUnknownField()
+	{
+        $mock = $this->getMockBuilder(FieldMapperTrait::class)
+            ->getMockForTrait();
+
+        $this->expectException(MapperNotRegisteredException::class);
+
+		$mock->getFieldMapper('foo');
+	}
+
+    /**
+     * @covers ::getFieldMapper
+     * @group mapper
+     *
+     * @return void
+     */
+	public function testGetFieldMapperReturnsCorrectFieldMapper()
+	{
+        $mock = $this->getMockBuilder(FieldMapperTrait::class)
+            ->getMockForTrait();
+
+
+        $mapper = $this->getMockBuilder(FieldMapper::class)
+            ->setMethods(['getSupportedFields', 'mapField'])
+            ->getMock();
+
+        $mapper->expects($this->once())
+            ->method('getSupportedFields')
+            ->will(
+                $this->returnValue(array('foo'))
+            );
+
+        $mock->registerFieldMapper($mapper);
+
+		$result = $mock->getFieldMapper('foo');
+
+		$this->assertSame(
+			$mapper,
+			$result
+		);
+	}
 }
