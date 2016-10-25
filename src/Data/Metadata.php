37e693795ca459e4eace4b313608eec2ec130d48
Tom Van Herreweghe <tom@theanalogguy.be>
2016-10-25 21:07:41 +0200

    Move adapter-related code & interfaces to this repo
    Signed-off-by: Tom Van Herreweghe <tom@theanalogguy.be>

---
+++ src/Data/Metadata.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Exif: A container class for EXIF data
+ *
+ * @link        http://github.com/PHPExif/php-exif-common for the canonical source repository
+ * @copyright   Copyright (c) 2016 Tom Van Herreweghe <tom@theanalogguy.be>
+ * @license     http://github.com/PHPExif/php-exif-common/blob/master/LICENSE MIT License
+ * @category    PHPExif
+ * @package     Common
+ */
+
+namespace PHPExif\Common\Data;
+
+/**
+ * Metadata class
+ *
+ * Container for Metadata of an image
+ *
+ * @category    PHPExif
+ * @package     Common
+ */
+final class Metadata implements MetadataInterface
+{
+    /**
+     * @var ExifInterface
+     */
+    private $exif;
+
+    /**
+     * @var IptcInterface
+     */
+    private $iptc;
+
+    /**
+     * Constructor
+     *
+     * @param ExifInterface $exif
+     * @param IptcInterface $iptc
+     */
+    public function __construct(ExifInterface $exif, IptcInterface $iptc)
+    {
+        $this->exif = $exif;
+        $this->iptc = $iptc;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function withExif(ExifInterface $exif)
+    {
+        $new = clone $this;
+        $new->exif = $exif;
+
+        return $new;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function withIptc(IptcInterface $iptc)
+    {
+        $new = clone $this;
+        $new->iptc = $iptc;
+
+        return $new;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getExif()
+    {
+        return $this->exif;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getIptc()
+    {
+        return $this->iptc;
+    }
+}
---
+++ src/Mapper/MapperAccessorTrait.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Accessor & Mutator for Mapper
+ *
+ * @link        http://github.com/PHPExif/php-exif-common for the canonical source repository
+ * @copyright   Copyright (c) 2016 Tom Van Herreweghe <tom@theanalogguy.be>
+ * @license     http://github.com/PHPExif/php-exif-common/blob/master/LICENSE MIT License
+ * @category    PHPExif
+ * @package     Common
+ */
+
+namespace PHPExif\Common\Mapper;
+
+use PHPExif\Common\Adapter\MapperInterface;
+use PHPExif\Common\Exception\Mapper\NoMapperSetException;
+
+/**
+ * MapperAccessorTrait
+ *
+ * @category    PHPExif
+ * @package     Common
+ */
+trait MapperAccessorTrait
+{
+    /**
+     * @var MapperInterface
+     */
+    private $mapper;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getMapper()
+    {
+        if (null !== $this->mapper) {
+            return $this->mapper;
+        }
+
+        throw new NoAdapterSetException;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setMapper(MapperInterface $mapper)
+    {
+        $this->mapper = $mapper;
+    }
+}
